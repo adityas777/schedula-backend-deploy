a9b8f019ecd3f492134cc1a1aa4a1be67ce1c108
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-29 15:43:33 +0530

    Update configuration.ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,8 @@
 export default () => ({
-  PORT: parseInt(process.env.PORT, 10) || 3000,
-  JWT_SECRET: process.env.JWT_SECRET || 'default_secret',
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: parseInt(process.env.DB_PORT, 10),
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
+  PORT: parseInt(process.env.PORT ?? '3000'),
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
 });
