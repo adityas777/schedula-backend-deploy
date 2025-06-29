5414e73fbd54313f34a7edb623acfcd59e1d789b
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-29 15:38:13 +0530

    Update jwt.strategy.ts

--- src/auth/strartigies/jwt.strategy.ts
+++ src/auth/strartigies/jwt.strategy.ts
@@ -1,18 +1,18 @@
-
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(cfg: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: cfg.get<string>('JWT_SECRET'),
     });
   }
 
-  async validate(payload: any) {
-    return payload;
+  validate(payload: any) {
+    return payload; // { sub, email, role }
   }
 }
