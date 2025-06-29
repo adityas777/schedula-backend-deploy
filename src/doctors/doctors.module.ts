0cc71bf08dc60f2b7e70e1c6a81ac650889e2b68
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-29 15:41:30 +0530

    Update doctors.module.ts

--- src/doctors/doctors.module.ts
+++ src/doctors/doctors.module.ts
@@ -1,13 +1,12 @@
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Doctor } from './doctor.entity';
-import { DoctorsController } from './doctors.controller';
 import { DoctorsService } from './doctors.service';
+import { DoctorsController } from './doctors.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Doctor])],
-  controllers: [DoctorsController],
   providers: [DoctorsService],
+  controllers: [DoctorsController],
 })
 export class DoctorsModule {}
