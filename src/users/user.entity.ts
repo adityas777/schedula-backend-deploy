562e19f159db6f743a0d26387356babf9ad9c7aa
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-29 15:41:06 +0530

    Update user.entity.ts

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,23 +1,15 @@
-
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export enum Provider { LOCAL = 'local', GOOGLE = 'google' }
+export enum Role { DOCTOR = 'doctor', PATIENT = 'patient' }
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryGeneratedColumn('uuid') id: string;
 
-  @Column({ unique: true })
-  email: string;
-
-  @Column()
-  name: string;
-
-  @Column({ nullable: true })
-  password: string;
-
-  @Column()
-  provider: 'local' | 'google';
-
-  @Column()
-  role: 'doctor' | 'patient';
+  @Column({ unique: true }) email: string;
+  @Column() name: string;
+  @Column({ nullable: true }) password: string;
+  @Column({ type: 'enum', enum: Provider }) provider: Provider;
+  @Column({ type: 'enum', enum: Role }) role: Role;
 }
